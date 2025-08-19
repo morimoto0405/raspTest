207b4ece9683b98f9a1205d03f3a87b19480eca4
morimoto <morimoto@kotera-ss.jp>
2025-08-19 17:41:12 +0900

    初めてのコミッ

--- app.js
+++ app.js
@@ -3,10 +3,6 @@ const app = express();
 const path = require("path");
 const { exec, spawn } = require("child_process");
 const fs = require("fs");
-const http = require("http");
-const { Server } = require("socket.io");
-const server = http.createServer(app);
-const io = new Server(server);
 
 app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "views"));
@@ -14,42 +10,32 @@ app.set("view engine", "ejs");
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ extended: true }));
 
-function startGStreamer(){
-    const gst = spawn("gst-launch-1.0",[
-        "-v",
-        "libcamerasrc",
-        "!", "videoconvert",
-        "!", "vp8enc", "deadline=1",
-        "!", "rtpvp8pay",
-        "!", "webrtcbin", "name=sendrecv", "stun-server=stun://stun.l.google.com:19302"
-    ]);
-
-    gst.stdout.on("data", data => console.log("GStreamer:", data.toString()));
-    gst.stderr.on("data", data => console.error("GStreamer ERR:", data.toString()));
-    gst.on("close", code => console.log("GStreamer exited with code", code));
-
-    return gst;
-}
-
-const gstProcess = startGStreamer();
-
 app.get("/", (req,res)=>{
     res.render("camera");
 });
 
-io.on("connection", socket=>{
-    console.log("Client connected");
-    socket.on("offer",(sdp)=>{
-        socket.emit("answer", sdp);
+app.get("/stream", (req,res)=>{
+    res.writeHead(200,{
+       "Content-Type": "multipart/x-mixed-replace; boundary=frame",
+        "Cache-Control": "no-cache",
+        "Connection": "close", 
     });
 
-    socket.on("ice-candidate", (candidate)=>{
-        socket.broadcast.emit("ice-candidate", candidate);
+    const cam = spawn("libcamera-jpeg",[
+        "--width", "640",
+        "--height", "480",
+        "--framerate", "30",
+        "--output", "-"
+    ]);
+
+    cam.stdout.on("data", (data)=>{
+        res.write("--frame\r\nContent-Type: image/jpeg\r\n\r\n");
+        res.write(data);
+        res.write("\r\n");
     });
 
-    socket.on("disconnect", ()=>{
-        console.log("client disconnected");
-    });
+    cam.stderr.on("data", (data)=> console.error("Camera Err:", data.toString()));
+    req.on("close", ()=> cam.kill());
 });
 
 app.get("/photo", (req,res)=>{
@@ -65,7 +51,34 @@ app.get("/photo", (req,res)=>{
     });
 });
 
+// 動画録画開始
+app.get("/video/start", (req, res) => {
+  if (videoProcess) return res.send("録画中です");
+  const filename = `video_${Date.now()}.h264`;
+  const filePath = path.join(__dirname, "public", "videos", filename);
 
-server.listen(3000, ()=>{
+  videoProcess = spawn("libcamera-vid", [
+    "--width", "1280",
+    "--height", "720",
+    "--framerate", "30",
+    "--timeout", "0",
+    "--output", filePath
+  ]);
+
+  videoProcess.stderr.on("data", (data) => console.error("Video ERR:", data.toString()));
+  videoProcess.on("close", () => { videoProcess = null; });
+  res.send(`録画開始: ${filename}`);
+});
+
+// 動画録画停止
+app.get("/video/stop", (req, res) => {
+  if (!videoProcess) return res.send("録画中ではありません");
+  videoProcess.kill();
+  videoProcess = null;
+  res.send("録画停止");
+});
+
+
+app.listen(3000, ()=>{
     console.log("ポート3000で待受け中");
 });
